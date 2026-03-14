a99ff422eddb3fc3223008c832b15b73cfef3c06
laxsolanki <lalitksolanki123@gmail.com>
2026-03-14 20:31:52 +0530

    Refactor student record system with improved structure

--- main.c
+++ main.c
@@ -1,44 +1,184 @@
 #include<stdio.h>
-int main(){
-  int n;
-  scanf("%d",&n);
-  int password=1007;
-  if(n==password){
+#include<string.h>
 
-int roll[100],Class[100],count=0;
-char name[100][50],father[100][50];    //float marks[100];
+int main(){
+
+FILE *fp,*temp;
+
+int n;
+printf("Enter password:\n ");
+scanf("%d\n",&n);
+
+int password = 1007;
+
+if(n == password){
+
+int roll, cls;
+char name[50], father[50], mother[50], batch[20];
 
 while(1){
-        int choise;
 
-printf(".....Student Record system..... \n" ) ;
-printf( "1 add student information \n ");
+int choice;
 
-printf("2 view All student \n ");
-printf("3 search student \n  ") ;
-printf("4 all student information \n ");
-printf("5 Exit \n "  );
-scanf("%d",&choise) ;
-if(choise ==1){
-        printf("Enter a roll number");
-    scanf("%d",roll[count]);
-    printf("Enter a name ");
-    scanf("%s",name[count]);
-printf("enter father name \n ");
-    scanf("%s" ,father[count]);
-    printf("Enter mother name\n");
-    scanf("%s",mother[count]);
-    printf("Enter class\n");
-    scanf("%d",&Class[count]);
+printf("\n.....Student Record System.....\n");
+printf("1 Add student information\n");
+printf("2 View All students\n");
+printf("3 Search student\n");
+printf("4 Delete student\n");
+printf("5 Exit\n");
 
-    printf("...succesfull...");
-    count++;
+printf("Enter choice:\n ");
+scanf("%d\n",&choice);
+
+if(choice == 1){
+
+fp = fopen("students.txt","a");
+
+printf("Enter name:\n ");
+scanf("%s\n",name);
+
+printf("Enter roll number:\n ");
+scanf("%d\n",&roll);
+
+printf("Enter father name:\n ");
+scanf("%s\n",father);
+
+printf("Enter mother name:\n ");
+scanf("%s\n",mother);
+
+printf("Enter class:\n ");
+scanf("%d\n",&cls);
+
+printf("Enter batch:\n ");
+scanf("%s\n",batch);
+
+fprintf(fp,"%d %s %s %s %d %s\n",
+roll,name,father,mother,cls,batch);
+
+fclose(fp);
+
+printf("Student saved successfully\n");
+
 }
+
+else if(choice == 2){
+
+fp = fopen("students.txt","r");
+
+if(fp == NULL){
+printf("No records found\n");
 }
-  }
-  else{
-    printf("Invalid password");
-  }
+
+else{
+
+printf("\nRoll Name Father Mother Class Batch\n");
+
+while(fscanf(fp,"%d %s %s %s %d %s",
+&roll,name,father,mother,&cls,batch)!=EOF){
+
+printf("%d %s %s %s %d %s\n",
+roll,name,father,mother,cls,batch);
+
+}
+
+fclose(fp);
+
+}
+
+}
+
+else if(choice == 3){
+
+int r, found=0;
+
+printf("Enter roll number:\n ");
+scanf("%d\n",&r);
+
+fp = fopen("students.txt","r");
+
+while(fscanf(fp,"%d %s %s %s %d %s",
+&roll,name,father,mother,&cls,batch)!=EOF){
+
+if(r == roll){
+
+printf("\nStudent Found\n");
+printf("Name: %s\n",name);
+printf("Father: %s\n",father);
+printf("Mother: %s\n",mother);
+printf("Class: %d\n",cls);
+printf("Batch: %s\n",batch);
+
+found=1;
+
+}
+
+}
+
+if(found==0){
+printf("Record not found\n");
+}
+
+fclose(fp);
+
+}
+
+else if(choice == 4){
+
+int r, found=0;
+
+printf("Enter roll number to delete: ");
+scanf("%d",&r);
+
+fp = fopen("students.txt","r");
+temp = fopen("temp.txt","w");
+
+while(fscanf(fp,"%d %s %s %s %d %s",
+&roll,name,father,mother,&cls,batch)!=EOF){
+
+if(roll != r){
+
+fprintf(temp,"%d %s %s %s %d %s\n",
+roll,name,father,mother,cls,batch);
+
+}
+
+else{
+found = 1;
+}
+
+}
+
+fclose(fp);
+fclose(temp);
+
+remove("students.txt");
+rename("temp.txt","students.txt");
+
+if(found==1){
+printf("Student deleted successfully\n");
+}
+else{
+printf("Record not found\n");
+}
+
+}
+
+else if(choice == 5){
+printf("Program Exit\n");
+break;
+}
+
+else{
+printf("Invalid choice\n");
+}
+
+}
+
+}
+
+else{
+printf("Invalid password\n");
+}
 
 return 0;
 }
